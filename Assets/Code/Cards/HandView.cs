f1b8bb331e930867fe276ce08db83ba9dbd3001c
Livonski <rozernoj@gmail.com>
2024-12-19 14:30:48 +0200

    Better card placement
    -Improved card placement, by placing it on an arch instead of just one position

--- Assets/Code/Cards/HandView.cs
+++ Assets/Code/Cards/HandView.cs
@@ -7,6 +7,9 @@ public class HandView : MonoBehaviour
     [SerializeField] private Transform _cardParent;
     [SerializeField] private GameObject _cardPrefab;
 
+    [SerializeField] private float _arcAngle;
+    [SerializeField] private float _arcRadius;
+
     private Hand _playerHand;
 
     public void Initialize(Hand hand)
@@ -24,13 +27,27 @@ public class HandView : MonoBehaviour
             Destroy(child.gameObject);
         }
 
-        foreach(var card in cards)
+        if (cards.Count == 0) return;
+
+        int count = cards.Count;
+        float startAngle = -_arcAngle / 2f;
+        float angleStep = count == 1 ? 0 : _arcAngle / (count - 1);
+
+        for (int i = 0; i < count; i++)
         {
             GameObject cardGO = Instantiate(_cardPrefab, _cardParent);
             CardView cardView = cardGO.GetComponent<CardView>();
-
             if (cardView != null)
-                cardView.Setup(card.GetCardData());
+            {
+                cardView.Setup(cards[i].GetCardData());
+            }
+
+            float angle = startAngle + angleStep * i;
+            float rad = angle * Mathf.Deg2Rad;
+            Vector3 pos = new Vector3(Mathf.Sin(rad) * _arcRadius, Mathf.Cos(rad) * _arcRadius, 0f);
+
+            cardGO.transform.localPosition = pos;
+            cardGO.transform.localEulerAngles = new Vector3(0f, 0f, -angle);
         }
     }
 }
