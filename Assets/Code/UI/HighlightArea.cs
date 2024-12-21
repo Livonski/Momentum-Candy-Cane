6901ac603b444f15fee80d048c6b73d83f3320c7
Livonski <rozernoj@gmail.com>
2024-12-22 00:14:48 +0200

    Turn display follow
    Made so turn display follows players position

--- Assets/Code/UI/HighlightArea.cs
+++ Assets/Code/UI/HighlightArea.cs
@@ -35,11 +35,7 @@ public class HighlightArea : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
 
     public void PositionArea(Vector2 playerPosition, Vector2 forward, float offset)
     {
-        //Currently this stuff is not working properly
-        //I'll need to rethink it later
-        //The idea is to shift an object to the player position
-        //But i think rotation is messing things up
-        /*Vector2 perp;
+        Vector2 perp;
         if (_side == HighlightSide.Left)
         {
             perp = new Vector2(-forward.y, forward.x);
@@ -49,14 +45,10 @@ public class HighlightArea : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
             perp = new Vector2(forward.y, -forward.x);
         }
         perp.Normalize();
-
-        float halfWidth = _rectTransform.rect.width * _rectTransform.lossyScale.x * 0.5f;
-        float halfHeight = _rectTransform.rect.height * _rectTransform.lossyScale.x * 0.5f;
         Vector2 worldPos = playerPosition + perp * offset;
 
         Camera camera = Camera.main;
         Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(camera, worldPos);
-        screenPos += perp * _rectTransform.rect.height;
 
         Vector2 anchoredPos;
         RectTransformUtility.ScreenPointToLocalPointInRectangle(
@@ -64,15 +56,11 @@ public class HighlightArea : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
             screenPos,
             null,
             out anchoredPos);
+        _rectTransform.anchoredPosition = anchoredPos;
 
-        _rectTransform.anchoredPosition = anchoredPos;*/
-
-        float angle = Mathf.Atan2(forward.x, forward.y) * 180 / Mathf.PI - 90;
+        float angle = Mathf.Atan2(forward.y, forward.x) * (180 / Mathf.PI) + 180;
         _rectTransform.localEulerAngles = new Vector3(0f, 0f, angle);
-
-        //TODO: figure out how I want to change text position to be perfectly centered
         _textGO.GetComponent<RectTransform>().localEulerAngles = new Vector3(0f, 0f, -angle);
-        //Debug.Log($"{_side.ToString()} highlight area: perp:{perp}, worldPos: {worldPos}, screenPos: {screenPos}, anchoredPos: {anchoredPos}, playerPos: {playerPosition}. forward: {forward}");
     }
 
     public void BeginListening()
