1dd64acac2065aae99a5078b70f2e7a166ba5598
Livonski <rozernoj@gmail.com>
2024-12-23 22:05:42 +0200

    json walls positions
    -Moved walls positions storing to json file

--- Assets/Code/GameController.cs
+++ Assets/Code/GameController.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    [SerializeField] private TestObjectSpawnData[] _testObjects;
     [SerializeField] private Map _map;
     [SerializeField] private Player _player;
     [SerializeField] private int _initialCards;
@@ -14,12 +14,20 @@ public class GameController : MonoBehaviour
     [SerializeField] private int _totalCrossedLine = 0;
 
     [SerializeField] private EndMenu _endMenu;
+
+    [SerializeField] private TestObjectSpawnData[] _objects;
+    [SerializeField] private GameObject _wallGameObject;
+    private List<Vector2Int> _wallPositions;
+
     private void Start()
     {
         if (_map == null)
             _map = GameObject.FindGameObjectWithTag("Map").GetComponent<Map>();
         _map.GenerateMap();
-        SpawnTestObjects();
+
+        LoadWallData();
+        SpawnWalls();
+        SpawnObjects();
         DrawInitialCards();
     }
 
@@ -39,9 +47,36 @@ public class GameController : MonoBehaviour
         _totalCrossedLine++;
     }
 
-    private void SpawnTestObjects()
+    private void LoadWallData()
     {
-        foreach (var data in _testObjects)
+        string path = Path.Combine(Application.dataPath, "walls.json");
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            var wallDataWrapper = JsonUtility.FromJson<WallDataWrapper>(json);
+            _wallPositions = wallDataWrapper.WallPositions;
+        }
+        else
+        {
+            Debug.LogError("Файл walls.json не найден");
+            _wallPositions = new List<Vector2Int>();
+        }
+    }
+
+    private void SpawnWalls()
+    {
+        int ID = 0;
+        foreach (var position in _wallPositions)
+        {
+            var initializationData = new InitializationData(position,ID,"Wall");
+            _map.SpawnObject(_wallGameObject, initializationData);
+            ID++;
+        }
+    }
+
+    private void SpawnObjects()
+    {
+        foreach (var data in _objects)
         {
             _map.SpawnObject(data.obj, data.initializationData);
         }
@@ -60,4 +95,11 @@ public class GameController : MonoBehaviour
         public GameObject obj;
         public InitializationData initializationData;
     }
+}
+
+
+[System.Serializable]
+public class WallDataWrapper
+{
+    public List<Vector2Int> WallPositions;
 }
